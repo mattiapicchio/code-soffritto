93ac960c9729f2dcd490cdf3628452ed4f9522cf
Mattia Picchio <picchiomattia@gmail.com>
2025-08-09 23:25:42 +0200

    feat: install and configure Tanstack Query

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,38 @@
+import { QueryClient } from '@tanstack/react-query';
 import { createRouter as createTanStackRouter } from '@tanstack/react-router';
+import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query';
+import { DefaultCatchBoundary } from './components/layout/DefaultCatchBoundary';
+import { NotFound } from './components/layout/NotFound';
 import { routeTree } from './routeTree.gen';
+import { minutes } from './utils/timeUtils';
 
 export function createRouter() {
-	const router = createTanStackRouter({
-		routeTree,
-		scrollRestoration: true,
-		defaultPreload: 'intent',
-		defaultPreloadStaleTime: 0, // Bypass the built-in caching. Overrode by Tanstack Query
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				// The duration until a query transitions from fresh to stale. As long as the query is fresh, data will always be read from the cache only.
+				staleTime: minutes(5),
+				// Queries that fail are silently retried 3 times, with exponential backoff delay
+				retry: 3,
+				refetchOnWindowFocus: false,
+				// The duration until inactive queries will be removed from the cache and garbage collected.
+				gcTime: minutes(10),
+			},
+		},
 	});
 
+	const router = createTanStackRouter({
+		routeTree,
+		context: { queryClient },
+		scrollRestoration: true,
+		defaultPreload: 'intent',
+		defaultErrorComponent: DefaultCatchBoundary,
+		defaultNotFoundComponent: () => <NotFound />,
+	});
+	setupRouterSsrQueryIntegration({
+		router,
+		queryClient,
+	});
 	return router;
 }
 
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,23 @@
 /// <reference types="vite/client" />
-import "@/assets/css/global.css";
-import { DefaultCatchBoundary } from '@/components/layout/DefaultCatchBoundary';
-import { NotFound } from '@/components/layout/NotFound';
+
+import type { QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import {
-	createRootRoute,
+	createRootRouteWithContext,
 	HeadContent,
 	Outlet,
 	Scripts,
 } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import type { ReactNode } from 'react';
+import globalCss from '@/assets/css/global.css?url';
+import { DefaultCatchBoundary } from '@/components/layout/DefaultCatchBoundary';
+import { NotFound } from '@/components/layout/NotFound';
+import { seo } from '@/utils/seo';
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+	queryClient: QueryClient;
+}>()({
 	head: () => ({
 		meta: [
 			{
@@ -21,9 +27,33 @@ export const Route = createRootRoute({
 				name: 'viewport',
 				content: 'width=device-width, initial-scale=1',
 			},
+			...seo({
+				title:
+					'Code Soffritto - A Type-Safe, Client-First, Full-Stack React Framework',
+				description: `Code Soffritto is a type-safe, client-first, full-stack React framework. `,
+			}),
+		],
+		links: [
+			{ rel: 'stylesheet', href: globalCss },
 			{
-				title: 'TanStack Start Starter',
+				rel: 'apple-touch-icon',
+				sizes: '180x180',
+				href: '/apple-touch-icon.png',
 			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '32x32',
+				href: '/favicon-32x32.png',
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '16x16',
+				href: '/favicon-16x16.png',
+			},
+			{ rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
+			{ rel: 'icon', href: '/favicon.ico' },
 		],
 	}),
 	errorComponent: (props) => {
@@ -47,14 +77,14 @@ function RootComponent() {
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
 	return (
-		<html lang='en' data-theme="dark">
+		<html lang='en' data-theme='dark'>
 			<head>
 				<HeadContent />
 			</head>
-			<body className='font-poppins bg-white text-black dark:bg-nightshade dark:text-sunflare'>
+			<body>
 				{children}
-				<TanStackRouterDevtools position="bottom-right" />
-        {/* <ReactQueryDevtools buttonPosition="bottom-left" /> */}
+				<TanStackRouterDevtools position='bottom-right' />
+				<ReactQueryDevtools buttonPosition='bottom-left' />
 				<Scripts />
 			</body>
 		</html>
